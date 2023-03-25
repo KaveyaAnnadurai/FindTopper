3d42bd52af964649003f2cfe746cc06ab40e56f6
KaveyaAnnadurai <kaveya.a@m2pfintech.com>
2023-03-25 15:38:54 +0530

    Add.Included the logic to find topper with roll number.

--- src/FindTopperStudent.java
+++ src/FindTopperStudent.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class FindTopperStudent {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int roll, temp, sumOfEvenDigits = 0, sumOfOddDigits = 0;
+
+        System.out.println("Enter roll no:");
+        roll = scanner.nextInt();
+
+        while(roll > 0){
+            temp = roll % 10;
+
+            if(temp%2 == 0)
+                sumOfEvenDigits += temp;
+            else sumOfOddDigits += temp;
+
+            roll = roll/10;
+
+        }
+
+        if(sumOfEvenDigits == sumOfOddDigits)
+            System.out.println("Yes");
+        else System.out.println("No");
+
+
     }
 }
